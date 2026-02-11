9f44bf0f2e39ff737b4e7285e8ece91744a88d9e
Pablo <pablodmachador@gmail.com>
2026-02-11 19:59:27 -0400

    pequeño fix en el sign
    mal llamado de ruta de URL

--- src/frontend/assets/js/registro.js
+++ src/frontend/assets/js/registro.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const apellido = nameParts.slice(1).join(' ') || 'S/A'; // 'S/A' si no pone apellido
 
         try {
-            const respuesta = await fetch('http://localhost:3000/api/estudiantes/registrar', {
+            const respuesta = await fetch('http://localhost:3000/api/estudiantes/registro', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
